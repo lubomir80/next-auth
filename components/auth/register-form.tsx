8f2ca724c84bbbda6d8220b38b3e74869d718510
Lubomir <100048089+lubomir80@users.noreply.github.com>
2024-12-12 17:50:13 +0100

    register page

--- components/auth/register-form.tsx
+++ components/auth/register-form.tsx
@@ -1,10 +1,9 @@
 "use client"
-
 import * as z from "zod"
 import CardWrapper from "./card-wrapper"
 import { useForm } from "react-hook-form"
 import { zodResolver } from "@hookform/resolvers/zod"
-import { LoginSchema } from "@/schemas"
+import { RegisterSchema } from "@/schemas"
 import {
    Form,
    FormControl,
@@ -18,45 +17,67 @@ import { Button } from "../ui/button"
 import FormError from "../form-error"
 import FormSuccess from "../form-success"
 import { useState, useTransition } from "react"
-import { login } from "@/lib/actions"
+import { register } from "@/lib/actions"
 
-function LoginForm() {
+
+function RegisterForm() {
    const [error, setError] = useState<string | undefined>("")
    const [success, setSuccess] = useState<string | undefined>("")
    const [isPending, startTransition] = useTransition()
 
-   const form = useForm<z.infer<typeof LoginSchema>>({
-      resolver: zodResolver(LoginSchema),
+   const form = useForm<z.infer<typeof RegisterSchema>>({
+      resolver: zodResolver(RegisterSchema),
       defaultValues: {
          email: "",
-         password: ""
+         password: "",
+         name: ""
       }
    })
 
-   const onSubmit = (values: z.infer<typeof LoginSchema>) => {
+   const onSubmit = (values: z.infer<typeof RegisterSchema>) => {
       setError("");
       setSuccess("")
 
-
       startTransition(() => {
-         login(values).then((data) => {
+         register(values).then((data) => {
             setError(data.error);
             setSuccess(data.success)
          })
       })
+
    }
+
+
 
 
    return (
       <CardWrapper
-         headerLabel="Welcome back"
-         backButtonLabel="Don't have an account"
-         backButtonHref="/auth/register"
-         showSocial>
+         headerLabel="Create an account"
+         backButtonLabel="Already have an account?"
+         backButtonHref="/auth/login"
+         showSocial
+      >
          <Form {...form}>
             <form onSubmit={form.handleSubmit(onSubmit)}
                className="space-y-6">
                <div className="space-y-4">
+                  <FormField
+                     control={form.control}
+                     name="name"
+                     render={({ field }) => (
+                        <FormItem>
+                           <FormLabel>Your name</FormLabel>
+                           <FormControl>
+                              <Input
+                                 disabled={isPending}
+                                 placeholder="user name"
+                                 {...field}
+                                 type="text" />
+                           </FormControl>
+                           <FormMessage />
+                        </FormItem>
+                     )}
+                  />
                   <FormField
                      control={form.control}
                      name="email"
@@ -91,11 +112,12 @@ function LoginForm() {
                         </FormItem>
                      )}
                   />
+
                </div>
                <FormError message={error} />
                <FormSuccess message={success} />
                <Button type="submit" className="w-full" disabled={isPending}>
-                  Login
+                  Register
                </Button>
             </form>
          </Form>
@@ -103,4 +125,4 @@ function LoginForm() {
    )
 }
 
-export default LoginForm
+export default RegisterForm
